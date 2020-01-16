3fba79b3b39c3c20648527088fa202889ca6d46e
yamaxim137 <59343015+yamaxim137@users.noreply.github.com>
2020-01-17 01:16:05 +0300

    третья версия

--- scrpt.js
+++ scrpt.js
@@ -1,1 +1,23 @@
+var term =
+ {
+		Ukr: 11,
+		Ru: -3,
+		By: 5,
+		Can: 0,
+		Uk: 8,
+		Ge: 13,
+		Sp: 23,
+	};
 
+var middl_temper = 0;
+var sum_temper = 0;
+var count = 0;
+
+for (prop in term) {
+	count++;
+	sum_temper += term.prop;
+}
+
+middl_temper = sum_temper / count;
+
+alert (term);
