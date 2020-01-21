8ca86b2bc72e7f15de5bd529b614744c69df8bc6
yamaxim137 <59343015+yamaxim137@users.noreply.github.com>
2020-01-21 16:23:23 +0300

    с функцией

--- scrpt.js
+++ scrpt.js
@@ -23,3 +23,33 @@ middl_temper = sum_temper / count;
 alert (middl_temper);
 // alert(count);
 
+function maxTemper(term) {
+	var middl_temper = 0;
+	var sum_temper = 0;
+	var count = 0;
+
+	for (let prop in term) {
+		sum_temper += term[prop];
+		count++;
+	}
+
+	middl_temper = sum_temper / count;
+
+	return middl_temper;
+}
+
+var i = maxTemper(term);
+alert (i);
+
+var term2 =
+ {
+		Ukr: 11,
+		Ru: -3,
+		By: 5,
+		Can: 0,
+		Uk: 8,
+		Sp: 23,
+	};
+
+i = maxTemper(term2);
+alert (i);
